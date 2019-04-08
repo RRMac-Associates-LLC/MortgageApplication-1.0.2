18e438924efc1ed9f14cc42ec505e3f2df215835
RRMac Associates, LLC <34245700+Bill-Pratt@users.noreply.github.com>
2019-04-08 15:49:57 -0400

    Update DynamicBuild.groovy

--- MortgageApplication/build/DynamicBuild.groovy
+++ MortgageApplication/build/DynamicBuild.groovy
@@ -21,7 +21,7 @@ Files.copy(source, target)
 def outFile = new File("$CODE_BASE_DIR/MortgageApplication/build/file.txt")
 
 //commitInfo=$($gitScript ls-remote $gitURL $gitBranch)
-def proc = "$gitScript ls-remote $gitURL $gitBranch".execute()"
+def proc = "$gitScript ls-remote $gitURL $gitBranch".execute()
 def b = new StringBuffer()
 proc.consumeProcessErrorStream(b)
 
