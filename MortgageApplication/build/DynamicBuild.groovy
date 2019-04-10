2577ffec5aa72cf1df32ad9a19db1683e01204eb
RRMac Associates, LLC <34245700+Bill-Pratt@users.noreply.github.com>
2019-04-10 12:02:22 -0400

    Update DynamicBuild.groovy

--- MortgageApplication/build/DynamicBuild.groovy
+++ MortgageApplication/build/DynamicBuild.groovy
@@ -5,12 +5,14 @@ def gitBranch="master"
 
 println("** in DynamicBuild.grooy **")
 
+
 println("** in DynamicBuild.grooy **")
 println("** CODE_BASE_DIR="+CODE_BASE_DIR)
 println("** gitScript="+gitScript)
 println("** gitURL="+gitURL)
 println("** gitBranch="+gitBranch)
 
+//backup files.txt that is coming in from git
 def now = new Date()
 def timestamp = now.format("yyyyMMdd-HHmmss.SSS", TimeZone.getTimeZone('UTC'))
 println("** copy file.txt to file-" + timestamp + ".txt")
@@ -21,29 +23,56 @@ src.renameTo "$CODE_BASE_DIR/MortgageApplication/build/files-" + timestamp + ".t
 
 //def outFile = new File("$CODE_BASE_DIR/MortgageApplication/build/files.txt")
 
+//get incoming commitID
 //commitInfo=$($gitScript ls-remote $gitURL $gitBranch)
 def proc = "$gitScript ls-remote $gitURL $gitBranch".execute()
 def commitIDs=proc.text.split()
 def commitID=commitIDs[0]
 println "commitID="+commitID
 
-println "$gitScript diff --name-only $commitID HEAD~1"
-def proc2 = "$gitScript diff --name-only $commitID HEAD~1".execute()
-def gitChanges=proc2.text.split()
+//need to check incoming commitID to last commitID that was built...this is saved in a file.  
+//if file does not exist, then get last commitID from git using HEAD~1
+File lastCommitIDBuiltFile = new File(CODE_BASE_DIR+'/lastCommitIDBuilt.txt')
+String previousCommitID = ""
 
-println "gitChanges="+gitChanges
-File outFile = new File("$CODE_BASE_DIR/MortgageApplication/build/files.txt")
-
-//outFile.withWriter{ out -> 
-//  gitChanges.each {out.println it.name}
-//}
-
-for(obj in gitChanges) 
+if(!lastCommitIDBuiltFile.exists() || lastCommitIDBuiltFile.text)
 {
-    println "obj="+obj
-    outFile << obj
+    previousCommitID = "HEAD~1"
+}
+else
+{
+    previousCommitID = lastCommitIDBuiltFile.text
 }
 
+println "previousCommitID="+previousCommitID
+
+
+if(previousCommitID != commitID)
+{    
+    //$gitScript diff --name-only $commitID HEAD~1
+    println "$gitScript diff --name-only $commitID $previousCommitID"
+    def proc2 = "$gitScript diff --name-only $commitID HEAD~1".execute()
+    def gitChanges=proc2.text.split()
+
+    println "gitChanges="+gitChanges
+    File outFile = new File("$CODE_BASE_DIR/MortgageApplication/build/files.txt")
+
+    //outFile.withWriter{ out -> 
+    //  gitChanges.each {out.println it.name}
+    //}
+
+    for(obj in gitChanges) 
+    {
+        println "obj="+obj
+        outFile << obj
+    }
+    
+    lastCommitIDBuiltFile << commitID
+}
+else
+{
+   println "!!!!!!! Commit already built - " + previousCommitID + " = " + commitID
+}
 
 
 
