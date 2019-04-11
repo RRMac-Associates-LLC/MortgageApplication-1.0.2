60ac3ffa5b43ebd8fc8c5e13a484ed07df1c62d0
RRMac Associates, LLC <34245700+Bill-Pratt@users.noreply.github.com>
2019-04-11 14:45:00 -0400

    Update epsmlist.cbl

--- MortgageApplication/cobol/epsmlist.cbl
+++ MortgageApplication/cobol/epsmlist.cbl
@@ -8,7 +8,7 @@
       *
       *    changed by bpratt 4-3-2019
       *    (C) 2008 IBM - JIM HILDNER RESERVED.
-      * comment added by bpratt 20190410 ...change 5
+      * comment added by bpratt 20190410 ...change 6
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SOURCE-COMPUTER. IBM-FLEX-ES.
