1715776d9d258906d19a31c38a6e1f09b9c81979
RRMac Associates, LLC <34245700+Bill-Pratt@users.noreply.github.com>
2019-04-11 15:09:55 -0400

    Update DynamicBuild.groovy

--- MortgageApplication/build/DynamicBuild.groovy
+++ MortgageApplication/build/DynamicBuild.groovy
@@ -4,27 +4,12 @@ def gitURL="git@github.com:RRMac-Associates-LLC/MortgageApplication-1.0.2.git"
 def gitBranch="master"
 
 println("** in DynamicBuild.grooy **")
-
-
-println("** in DynamicBuild.grooy **")
 println("** CODE_BASE_DIR="+CODE_BASE_DIR)
 println("** gitScript="+gitScript)
 println("** gitURL="+gitURL)
 println("** gitBranch="+gitBranch)
 
-//backup files.txt that is coming in from git
-def now = new Date()
-def timestamp = now.format("yyyyMMdd-HHmmss.SSS", TimeZone.getTimeZone('UTC'))
-//println("** copy file.txt to file-" + timestamp + ".txt")
-println("** copy file.txt to file-original.txt")
-def src = new File("$CODE_BASE_DIR/MortgageApplication/build/files.txt")
-//src.renameTo "$CODE_BASE_DIR/MortgageApplication/build/files-" + timestamp + ".txt"
-def dst = new File("$CODE_BASE_DIR/MortgageApplication/build/files-original.txt")
-dst << src.text
-//clean out files.txt
-src.withWriter { writer ->
-        writer.write("")
-}
+
 
 //def outFile = new File("$CODE_BASE_DIR/MortgageApplication/build/files.txt")
 
@@ -43,53 +28,71 @@ String previousCommitID = ""
 //test from command line
 // /usr/lpp/IBM/dbb/groovy-2.4.12/groovyz /u/jenkins/workspace/GIT_Project/testDBB1.0.2/MortgageApplication/build/DynamicBuild.groovy
 
-//set prevoiusCommitID to the needed type
+//If the lastCommitIDBuiltFile does not exist or is empty we need to build everything...assume it is first time running
 if(!lastCommitIDBuiltFile.exists() || lastCommitIDBuiltFile.text == '')
 {
-    previousCommitID = "HEAD~1"
+    //previousCommitID = "HEAD~1"
+        
+     println("** Previous commitID not found.  Assuming this is first time build, so build everything...")   
+        
 }
 else
 {
     previousCommitID = lastCommitIDBuiltFile.text
-}
+        
+    println "previousCommitID="+previousCommitID
 
-println "previousCommitID="+previousCommitID
+        if(previousCommitID != commitID)
+        {    
+            //backup files.txt that is coming in from git
+                def now = new Date()
+                def timestamp = now.format("yyyyMMdd-HHmmss.SSS", TimeZone.getTimeZone('UTC'))
 
-if(previousCommitID != commitID)
-{    
-    //$gitScript diff --name-only $commitID HEAD~1
-    println "$gitScript diff --name-only $commitID $previousCommitID"
-    def proc2 = "$gitScript diff --name-only $commitID $previousCommitID".execute()    
-    def gitChanges=proc2.text.split()
-    println "proc2.text="+gitChanges.length  
+                println("** copy file.txt to file-original.txt")
+                def src = new File("$CODE_BASE_DIR/MortgageApplication/build/files.txt")
+                //src.renameTo "$CODE_BASE_DIR/MortgageApplication/build/files-" + timestamp + ".txt"
+                def dst = new File("$CODE_BASE_DIR/MortgageApplication/build/files-original.txt")
+                dst << src.text
+                //clean out files.txt
+                src.withWriter { writer ->
+                        writer.write("")
+                }
+                
+            //$gitScript diff --name-only $commitID HEAD~1
+            println "$gitScript diff --name-only $commitID $previousCommitID"
+            def proc2 = "$gitScript diff --name-only $commitID $previousCommitID".execute()    
+            def gitChanges=proc2.text.split()
+            println "proc2.text="+gitChanges.length  
 
-    println "gitChanges="+gitChanges[]
-    File outFile = new File("$CODE_BASE_DIR/MortgageApplication/build/files.txt")
+            println "gitChanges="+gitChanges[]
+            File outFile = new File("$CODE_BASE_DIR/MortgageApplication/build/files.txt")
 
-    //outFile.withWriter{ out -> 
-    //  gitChanges.each {out.println it.name}
-    //}
+            //outFile.withWriter{ out -> 
+            //  gitChanges.each {out.println it.name}
+            //}
 
-    for(obj in gitChanges) 
-    {
-        println "obj="+obj
-        //outFile << obj
- 
-        outFile.withWriterAppend{ out ->
-                out.println obj
+            for(obj in gitChanges) 
+            {
+                println "obj="+obj
+                //outFile << obj
+
+                outFile.withWriterAppend{ out ->
+                        out.println obj
+                }
+            }
+
+            //update last commitID in file
+            lastCommitIDBuiltFile.withWriter { writer ->
+                writer.write(commitID)
+            }
         }
-    }
-    
-    //update last commitID in file
-    lastCommitIDBuiltFile.withWriter { writer ->
-        writer.write(commitID)
-    }
-     
+        else
+        {
+           println "!!!!!!! Commit already built, nothing to do - " + previousCommitID + " = " + commitID
+        }    
 }
-else
-{
-   println "!!!!!!! Commit already built - " + previousCommitID + " = " + commitID
-}
+
+
 
 
 
