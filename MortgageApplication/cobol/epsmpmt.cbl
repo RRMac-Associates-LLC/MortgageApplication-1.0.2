9de1dd5004af810192d3a32cc9f560acc7b36372
RRMac Associates, LLC <34245700+Bill-Pratt@users.noreply.github.com>
2019-06-13 10:50:54 -0400

    Update epsmpmt.cbl

--- MortgageApplication/cobol/epsmpmt.cbl
+++ MortgageApplication/cobol/epsmpmt.cbl
@@ -13,7 +13,7 @@
       *       INTEREST RATE
       *    OUTPUT:
       *       MONTHLY PAYMENT
-      *    19
+      *    20
       *    (C) 2008 IBM - Jim Hildner
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
