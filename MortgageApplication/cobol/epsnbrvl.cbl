c1858685f053fac0afa453916b8dbed7c3f2d054
RRMac Associates, LLC <34245700+Bill-Pratt@users.noreply.github.com>
2019-12-19 13:42:01 -0500

    Update epsnbrvl.cbl
    Updated to test dynamic build scripts

--- MortgageApplication/cobol/epsnbrvl.cbl
+++ MortgageApplication/cobol/epsnbrvl.cbl
@@ -14,7 +14,7 @@
       *       MONTHLY PAYMENT
       *
       *    (C) 2008 IBM - Jim Hildner
-      * comment added by bpratt 20190410 change 23
+      * comment added by bpratt 20190410 change 24
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
        SOURCE-COMPUTER. FLEX-ES.
